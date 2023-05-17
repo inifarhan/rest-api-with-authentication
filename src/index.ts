4f9c7f7020287edbcd1d4e32890785f43609eaef
inifarhan <farhangunawan619@gmail.com>
2023-05-17 19:48:56 +0700

    Creating user model

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose'
+
+const app = express()
+
+app.use(cors({
+    credentials:true
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log('Server running on http://localhost:8080')
+})
+
+const MONGO_URL = 'mongodb+srv://farhan:farhan@cluster0.uyd2es7.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error))
